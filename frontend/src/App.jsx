19c5081b123a1f6396ca51f6b14d7a640a0ca97d
masterJKR <nilzero25@gamil.com>
2026-05-18 16:11:51 +0900

    5월 18일

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import StudentPage from './page/StudentPage'
+
 
 function App() {
   
   return (
     <>
       <h1>학생 학습 분석 리포트 시스템</h1>
+      <StudentPage />
     </>
   )
 }
